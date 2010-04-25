e57777bebb31f9dd889f65406a3abbd1930aa7ca
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2010-04-25 14:37:17 +0800

    return a random string

--- lib/lao_wai_hua.rb
+++ lib/lao_wai_hua.rb
@@ -11,16 +11,52 @@ module LaoWaiHua
    打 翻 醋 ， 醋 湿 布 
   )
   
-  # return a string of *length* taken from the provided tongue twister
-  # defaults to the full length of the text
-  def self.generate(length=WORDS.length)
-    words = []
-	  while words.length < length
-	    words += LaoWaiHua::WORDS[0, length]
-	  end
-	  words.join("")
-  end
+  PUNCTUATION = %w( ，  。 )
+  
+  WORDS_WITHOUT_PUNCTUATION = WORDS.select{|word| !PUNCTUATION.include?(word)}
+  
+  class << self
+  
+    # return a string of *length* taken from the provided tongue twister
+    # defaults to the full length of the text
+    def generate(length=WORDS.length)
+  	  words = []
+  	  while words.length < length
+  		  words += WORDS[0, length]
+  	  end
+  	  words.join("")
+    end
 
+    # return a random word (excluding punctuation) from our text
+    def random_word()
+  	  WORDS_WITHOUT_PUNCTUATION[rand(WORDS_WITHOUT_PUNCTUATION.length)]
+    end
+  
+    # return a string or random words of the provided *length*
+    def random(length)
+  	  words = []
+      next_punctuation = nil
+      
+    	1.upto(length) do
+    	  next_punctuation ||= rand(3)+2
+    	  if next_punctuation == 0
+    	    next_punctuation = nil
+    	    words << random_punctuation
+    	  else
+    	    next_punctuation -= 1
+    	    words << random_word
+    	  end
+    	end
+    	words.join("")
+    end
+    
+    protected
+    
+    def random_punctuation()
+      PUNCTUATION[rand(PUNCTUATION.length)]
+    end
+
+  end
 end
 
 def LaoWaiHua(*args)
